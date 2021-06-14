e6522782b672ecb8d788b839da1f189069b72578
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-06-14 11:49:04 +0100

    Implemented logger

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,16 +1,20 @@
 from flask import Flask
+import logging
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info('Main Request Successful')
     return "Hello World!"
 
 @app.route("/status")
 def status():
+    app.logger.info('Status Request Successful')
     return {"result": "OK - healthy"}, 200
 
 @app.route("/metrics")
 def metrics():
+    app.logger.info('Metric Request Successful')
     return {
         "data": {
             "UserCount": 140, 
@@ -19,4 +23,5 @@ def metrics():
         }, 200
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
